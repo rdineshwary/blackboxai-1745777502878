7c82c38bc7747be812fd74ba1cd4e69d0c9d9c5b
rdineshwary <rdinehwary@gmail.com>
2025-04-28 02:49:01 +0800

    Add models.py 1745779742550

---
+++ models.py
@@ -0,0 +1,26 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150), nullable=False)
+    author = db.Column(db.String(100), nullable=False)
+    year = db.Column(db.Integer, nullable=True)
+    isbn = db.Column(db.String(20), unique=True, nullable=True)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+class BorrowRecord(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    book_id = db.Column(db.Integer, db.ForeignKey('book.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    borrow_date = db.Column(db.DateTime, default=datetime.utcnow)
+    return_date = db.Column(db.DateTime, nullable=True)
+
+    book = db.relationship('Book', backref=db.backref('borrow_records', lazy=True))
+    user = db.relationship('User', backref=db.backref('borrow_records', lazy=True))
